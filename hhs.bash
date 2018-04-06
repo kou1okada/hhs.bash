f43dc39fc27a3a790babe488a86bb049baedd1ec
kou1okada <kou1okada@users.noreply.github.com>
2018-04-06 21:25:43 +0900

    Added variables of the path informations (HHS_* and SCRIPT_*).

--- hhs.bash
+++ hhs.bash
@@ -9,13 +9,30 @@
 
 HHS_VERSION=0.0.1
 HHS_VERSION_COMPATIBILITY=0.0.1
-HHS_DIR="$(dirname "$(realpath "$BASH_SOURCE")")"
+
+HHS_PATH="$BASH_SOURCE"
+HHS_FILE="${HHS_PATH##*/}"
+HHS_NAME="${HHS_FILE%.*}"
+HHS_DIR="${HHS_PATH%/*}"
+HHS_REALPATH="$(readlink -f "$BASH_SOURCE")"
+HHS_REALFILE="${HHS_REALPATH##*/}"
+HHS_REALNAME="${HHS_REALFILE%.*}"
+HHS_REALDIR="${HHS_REALPATH%/*}"
+
+SCRIPT_PATH="$0"
+SCRIPT_FILE="${SCRIPT_PATH##*/}"
+SCRIPT_NAME="${SCRIPT_FILE%.*}"
+SCRIPT_DIR="${SCRIPT_PATH%/*}"
+SCRIPT_REALPATH="$(readlink -f "${SCRIPT_PATH}")"
+SCRIPT_REALFILE="${SCRIPT_REALPATH##*/}"
+SCRIPT_REALNAME="${SCRIPT_REALFILE%.*}"
+SCRIPT_REALDIR="${SCRIPT_REALPATH%/*}"
 
 function hhsinc () #= FILE
 {
   local -n include_guard="HHS_${1^^}_BASH"
   [ -n "$include_guard" ] && return
-  source "${HHS_DIR%%/}/hhs_${1}.bash"
+  source "${HHS_REALDIR%%/}/hhs_${1}.bash"
   include_guard=1
 }
 
