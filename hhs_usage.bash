96503241fab963cc1646c2c29564e1d1a52f6447
kou1okada <kou1okada@users.noreply.github.com>
2018-04-09 15:38:51 +0900

    Improved: `usage_default` became to generate summaries of the subcommand and descriptions of the option.

--- hhs_usage.bash
+++ hhs_usage.bash
@@ -1,7 +1,73 @@
 [ -n "${HHS_USAGE_BASH}" ] && return
 :     ${HHS_USAGE_BASH:=1}
 
+function usage__commands #= [CMD]
+{
+  local i lines src=()
+  
+  for (( i = ${#BASH_SOURCE[@]} - 1; 0 <= i; --i )); do
+    if cat "${BASH_SOURCE[i]}" | grep -qE "^function +${1:-$CMD}_([^ ]+) *\( *\)"; then
+      src+=( "${BASH_SOURCE[i]}" )
+    fi
+  done
+  
+  for i in "${src[@]}"; do
+    cat "$i" \
+    | grep -A1 -nE "^function +${1:-$CMD}_([^ ]+) *\( *\)" \
+    | awk '
+      match($0, /^[0-9]+:function +'${1:-$CMD}'_([^ ]+) *\( *\) *(# (.*))?/, m) {
+        s=s sprintf("  %s %s\n", gensub(/_/, "-", "g", m[1]), m[3]);
+      }
+      match($0, /^[0-9]+-(# (.*))/, m) {
+        s=s sprintf("    %s\n", m[2]);
+      }
+      END{
+        if (s != "") {
+          s="Commands:\n" s;
+        }
+        printf("%s",s);
+      }
+    '
+  done
+}
 
+function usage__options #= [CMD]
+{
+  local i lines src=()
+
+  for (( i = ${#BASH_SOURCE[@]} - 1; 0 <= i; --i )); do
+    if cat "${BASH_SOURCE[i]}" | grep -qE "^function +optparse_${1:-$CMD} *\( *\)"; then
+      src+=( "${BASH_SOURCE[i]}" )
+    fi
+  done
+  
+  for i in "${src[@]}"; do
+    readarray -t lines < <(
+      cat "$i" \
+      | grep -nE "^function +optparse_${1:-$CMD} *\( *\)|^{|^}" \
+      | grep -A2 -E "^[0-9]+:function +optparse_${1:-$CMD} +\( *\)" \
+      | sed -r -e 's/^([0-9]+).*/\1/g')
+    cat "$i" \
+    | head -n+$(( lines[2] - 1 )) \
+    | tail -n+$(( lines[1] + 1 )) \
+    | awk '
+      opt == 1 && match($0, /^ *(# (.*))/, m) {
+        s=s sprintf("      %s\n", m[2]);
+      }
+      {opt = 0;}
+      match($0, /^ *(-[^)]*)\) *(# ((\[)?(.*)))?/, m) {
+        s=s sprintf("  %s%s%s\n", gensub(/\|/, ", ", "g", m[1]), m[3] == "" ? "" : length(m[1]) == 2 ? m[4] == "[" ? m[4] : " " : m[4] "=", m[5]);
+        opt = 1;
+      }
+      END{
+        if (s != "") {
+          s="Options:\n" s;
+        }
+        printf("%s",s);
+      }
+    '
+  done
+}
 
 function usage_default () #= [CMD]
 #   Auto generate default usage from documentation
@@ -29,6 +95,9 @@ function usage_default () #= [CMD]
   | tail -n+$(( lines[0] )) \
   | sed -r -e 's/^function +([^ ]+)[^#]*(#=? *(.*))?/Usage: \1 \3/g' \
            -e 's/^#\?? (.*)/\1/g'
+  
+  usage__commands
+  usage__options
 }
 
 function invoke_usage () #= [CMD]
